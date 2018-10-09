d947c12b7d3fdf293e92979e05b1f2756b3aa245
Tung Vu <me@tungv.com>
2018-10-09 10:23:38 +0700

    init

---
+++ Ripple.js
@@ -0,0 +1,105 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Ripple extends React.Component {
+  state = { active: false, start: false };
+  ref = React.createRef();
+  timers = [];
+
+  buildRipple = () => {
+    const rect = this.ref.current.getBoundingClientRect();
+    const containerStyle = {
+      position: "fixed",
+      top: rect.top,
+      left: rect.left,
+      width: rect.width,
+      height: rect.height,
+      overflow: "hidden"
+    };
+
+    if (this.state.start) {
+      containerStyle.mouseEvent = "none";
+    }
+
+    const size = Math.max(rect.width, rect.height);
+
+    const defaultRippleStyle = {
+      width: 35,
+      height: 35,
+      background: "rgba(0, 0, 0, 0.3)",
+      position: "absolute",
+      left: this.state.x - rect.left,
+      top: this.state.y - rect.top,
+      transform: "translate(-50%, -50%)",
+      pointerEvents: "none",
+      borderRadius: "50%",
+      transition: "all 300ms linear"
+    };
+
+    const rippleStyle = {
+      ...defaultRippleStyle,
+      opacity: 0,
+      transform: "translate(-50%, -50%)",
+      width: size * 2,
+      height: size * 2
+    };
+
+    return (
+      <div style={containerStyle}>
+        <div style={this.state.start ? rippleStyle : defaultRippleStyle} />
+      </div>
+    );
+  };
+
+  componentWillUnmount() {
+    this.timers.forEach(timer => {
+      clearTimeout(timer);
+    });
+  }
+
+  render() {
+    const {
+      props: { children, disabled, onClick },
+      state: { active }
+    } = this;
+
+    return disabled ? (
+      children
+    ) : (
+      <React.Fragment>
+        {React.cloneElement(React.Children.only(children), {
+          ref: this.ref,
+          style: Object.assign({}, children.props.style || {}, {
+            cursor: "pointer"
+          }),
+          onClick: e => {
+            const { pageX: x, pageY: y } = e;
+
+            this.timers.push(
+              setTimeout(() => {
+                this.setState({ active: true, x, y });
+                this.timers.push(
+                  setTimeout(() => {
+                    this.setState({ active: false, start: false });
+                    this.timers.length = 0;
+                  }, 300)
+                );
+
+                Promise.resolve().then(() => {
+                  this.setState({ start: true });
+                });
+              }, 100) // avoid instant unmount
+            );
+
+            if (typeof onClick === "function") {
+              onClick(e);
+            }
+          }
+        })}
+        {active && ReactDOM.createPortal(this.buildRipple(), document.body)}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Ripple;
